d7f31cc08f50f3618a5d5b83ce8ceec25f903f8e
Hayotbek-Turgunov <hayotbekturgunov01gmail.com>
2024-09-13 23:51:27 +0500

    I love you, a little , a lot, passionately ... not at all

---
+++ src/_8kyu/_array/I Love you, a little, a lot,passionately ...not at all/ILoveYou.js
@@ -0,0 +1,12 @@
+function howMuchILoveYou(nbPetals) {
+  let arr = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
+
+  return arr[(nbPetals - 1) % arr.length];
+}
